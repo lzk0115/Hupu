d1045f08d0c5cf1473ff281841f2cf997ba0b6db
lv <15978805555@163.com>
2018-09-21 10:26:51 +0800

    new_test

--- Hupu/spiders/test.py
+++ Hupu/spiders/test.py
@@ -1,10 +1,11 @@
-import requests 
+import requests
 import logging
+import
 url = "https://bbs.hupu.com/bxj/highlights"
 html = requests.get(url)
 logging.basicConfig(filename='app.log',
                     level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(message)s',
                     datefmt='%Y-%m-%d')
-logging.debug(html.content)
+# logging.debug(html.content)
 print(html.text)
