e3464681e79371d8daef49e64d5ae7181a69d28c
lv <15978805555@163.com>
2018-10-02 07:55:34 +0800

    update

--- Hupu/pipelines.py
+++ Hupu/pipelines.py
@@ -6,28 +6,21 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import re
 
-re_blo = re.compile(r'<blockquote>.*</blockquote>')
-re_sma = re.compile(r'<small .*</small>')
-re_img = re.compile(r'<img.*>')
-re_div = re.compile(r'<.*div.*>')
-re_br = re.compile(r'<br>|</br>')
-re_p = re.compile(r'<p>|</p>')
-
 
 class TextPipeline(object):
     def __init__(self):
+        self.re_blo = re.compile(r'<blockquote>.*</blockquote>')
+        self.re_sma = re.compile(r'<small .*</small>')
+        self.re_tag = re.compile(r'<.*?>')
         self.fout = open('text', 'w', encoding='utf-8')
 
     def process_item(self, item, spider):
         if item['text']:
             text = item['text']
-            temp = re.sub(re_blo, '', text)
-            temp = re.sub(re_sma, '', temp)
-            temp = re.sub(re_div, '', temp)
-            temp = re.sub(re_br, '', temp)
-            temp = re.sub(re_p, '', temp)
-            temp = re.sub(re_img, '', temp)
-            temp = temp.replace('\n', '')
+            temp = text.replace('\n', '')
+            temp = re.sub(self.re_blo, '', temp)
+            temp = re.sub(self.re_sma, '', temp)
+            temp = re.sub(self.re_tag, '', temp)
             item['text'] = temp
             self.fout.write(temp + '\n')
         return item
